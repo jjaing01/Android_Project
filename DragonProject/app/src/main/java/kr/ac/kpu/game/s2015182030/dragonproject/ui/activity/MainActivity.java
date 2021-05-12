181d4ee3d7af32ea555b9cae1fe0320a782dea9d
Lee Dong Hee <jjaing01@naver.com>
2021-05-12 21:57:57 +0900

    Android Term Project Create Finish

---
+++ DragonProject/app/src/main/java/kr/ac/kpu/game/s2015182030/dragonproject/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.kpu.game.s2015182030.dragonproject.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.kpu.game.s2015182030.dragonproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ DragonProject/app/src/main/java/kr/ac/kpu/game/s2015182030/dragonproject/ui/view/GameView.java
@@ -0,0 +1,4 @@
+package kr.ac.kpu.game.s2015182030.dragonproject.ui.view;
+
+public class GameView {
+}
